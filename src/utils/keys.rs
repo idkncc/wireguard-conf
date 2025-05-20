b60b0f242b77678a9f870205c57cc5418f3e39e1
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2025-05-20 17:56:49 +0300

    feat: Zeroize, `.to_bytes()`/`.as_bytes()`, changed keys to be tuples...
    and added examples for keys.

--- src/utils/keys.rs
+++ src/utils/keys.rs
@@ -2,6 +2,7 @@ use core::fmt;
 
 use base64::prelude::*;
 use x25519_dalek::{PublicKey as XPublicKey, StaticSecret};
+use zeroize::{Zeroize, ZeroizeOnDrop};
 
 use crate::WireguardError;
 
@@ -11,40 +12,88 @@ use crate::WireguardError;
 ///
 /// # Implements
 ///
+/// - Implements [`Zeroize`] and [`ZeroizeOnDrop`] for clearing secrets from memory.
+/// - Implements [`TryFrom<&str>`] or [`TryFrom<String>`] for importing key from Base64 format.
 /// - Implements [`fmt::Display`] for exporting key in Wireguard's format.
 /// - Implements [`fmt::Debug`].
-#[derive(Clone)]
-pub struct PrivateKey {
-    secret: StaticSecret,
+///
+/// # Examples
+///
+/// ```
+/// # use wireguard_conf::prelude::*;
+/// # fn main() -> WireguardResult<()> {
+/// // generate new random key:
+/// let key = PrivateKey::random();
+///
+/// // import key:
+/// let imported_key = PrivateKey::try_from("sJkP2oorqrq49P6Ln25MWo3X04PxhB8k+RnJJnZ4gEo=")?;
+///
+/// // export key via `fmt::Display` trait:
+/// let exported_key = imported_key.to_string();
+///
+/// assert_eq!(exported_key, "sJkP2oorqrq49P6Ln25MWo3X04PxhB8k+RnJJnZ4gEo=".to_string());
+/// # Ok(())
+/// # }
+/// ```
+#[derive(Clone, Zeroize, ZeroizeOnDrop)]
+pub struct PrivateKey(StaticSecret);
+
+impl PrivateKey {
+    /// Generate new a random [`PrivateKey`]
+    #[must_use]
+    pub fn random() -> PrivateKey {
+        Self(StaticSecret::random())
+    }
 }
 
 impl PrivateKey {
-    /// Generate new random [`PrivateKey`]
+    /// View private key as byte array.
+    #[inline]
     #[must_use]
-    pub fn random() -> PrivateKey {
-        Self {
-            secret: StaticSecret::random(),
-        }
+    pub fn as_bytes(&self) -> &[u8; 32] {
+        self.0.as_bytes()
+    }
+
+    /// Convert private key to a byte array.
+    #[inline]
+    #[must_use]
+    pub fn to_bytes(&self) -> [u8; 32] {
+        self.0.to_bytes()
     }
 }
 
 impl fmt::Debug for PrivateKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("PrivateKey")
-            .field("secret", &self.to_string())
+        f.debug_tuple("PrivateKey")
+            .field(&self.to_string())
             .finish()
     }
 }
 
+/// Exporting key as base64 for Wireguard.
 impl fmt::Display for PrivateKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", BASE64_STANDARD.encode(self.secret.to_bytes()))
+        write!(f, "{}", BASE64_STANDARD.encode(self.as_bytes()))
     }
 }
 
 impl PartialEq for PrivateKey {
     fn eq(&self, other: &Self) -> bool {
-        self.secret.as_bytes() == other.secret.as_bytes()
+        self.as_bytes() == other.as_bytes()
+    }
+}
+
+impl TryFrom<&str> for PrivateKey {
+    type Error = WireguardError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let bytes: [u8; 32] = BASE64_STANDARD
+            .decode(value)
+            .map_err(|_| WireguardError::InvalidPrivateKey)?
+            .try_into()
+            .map_err(|_| WireguardError::InvalidPrivateKey)?;
+
+        Ok(Self(StaticSecret::from(bytes)))
     }
 }
 
@@ -52,15 +101,7 @@ impl TryFrom<String> for PrivateKey {
     type Error = WireguardError;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        let bytes: [u8; 32] = BASE64_STANDARD
-            .decode(value)
-            .map_err(|_| WireguardError::InvalidPrivateKey)?
-            .try_into()
-            .map_err(|_| WireguardError::InvalidPrivateKey)?;
-
-        Ok(Self {
-            secret: StaticSecret::from(bytes),
-        })
+        Self::try_from(value.as_str())
     }
 }
 
@@ -70,25 +111,74 @@ impl TryFrom<String> for PrivateKey {
 ///
 /// # Implements
 ///
+/// - Implements [`Zeroize`] and [`ZeroizeOnDrop`] for clearing secrets from memory.
+/// - Implements [`TryFrom<&str>`] or [`TryFrom<String>`] for importing key from Base64 format.
+/// - Implements [`From<&PrivateKey>`] for converting [`PrivateKey`] to [`PublicKey`].
 /// - Implements [`fmt::Display`] for exporting key in Wireguard's format.
 /// - Implements [`fmt::Debug`].
-/// - Implements [`From<&PrivateKey>`] for converting [`PrivateKey`] to [`PublicKey`].
-#[derive(Clone, PartialEq)]
-pub struct PublicKey {
-    key: XPublicKey,
+///
+/// # Examples
+///
+/// ```
+/// # use wireguard_conf::prelude::*;
+/// # fn main() -> WireguardResult<()> {
+/// // generate new random key:
+/// let private_key = PrivateKey::random();         // 1. generate private key
+/// let public_key = PublicKey::from(&private_key); // 2. get public key via `From<&PrivateKey>`
+///
+/// // import key:
+/// let imported_key = PublicKey::try_from("ijxpP+2xo+s77bfbm4QZzl6OyYP7sIOTutqngQSlZBs=")?;
+///
+/// // export key via `fmt::Display` trait:
+/// let exported_key = imported_key.to_string();
+///
+/// assert_eq!(exported_key, "ijxpP+2xo+s77bfbm4QZzl6OyYP7sIOTutqngQSlZBs=".to_string());
+/// # Ok(())
+/// # }
+/// ```
+#[derive(Clone, PartialEq, Zeroize, ZeroizeOnDrop)]
+pub struct PublicKey(XPublicKey);
+
+impl PublicKey {
+    /// Convert this public key to a byte array.
+    #[inline]
+    #[must_use]
+    pub fn to_bytes(&self) -> [u8; 32] {
+        self.0.to_bytes()
+    }
+
+    /// View this public key as a byte array.
+    #[inline]
+    #[must_use]
+    pub fn as_bytes(&self) -> &[u8; 32] {
+        self.0.as_bytes()
+    }
 }
 
 impl fmt::Debug for PublicKey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("PublicKey")
-            .field("key", &self.to_string())
-            .finish()
+        f.debug_tuple("PublicKey").field(&self.to_string()).finish()
     }
 }
 
+/// Export key in base64 format for Wireguard.
 impl fmt::Display for PublicKey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", BASE64_STANDARD.encode(self.key.to_bytes()))
+        write!(f, "{}", BASE64_STANDARD.encode(self.as_bytes()))
+    }
+}
+
+impl TryFrom<&str> for PublicKey {
+    type Error = WireguardError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let bytes: [u8; 32] = BASE64_STANDARD
+            .decode(value)
+            .map_err(|_| WireguardError::InvalidPublicKey)?
+            .try_into()
+            .map_err(|_| WireguardError::InvalidPublicKey)?;
+
+        Ok(Self(XPublicKey::from(bytes)))
     }
 }
 
@@ -96,22 +186,12 @@ impl TryFrom<String> for PublicKey {
     type Error = WireguardError;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        let bytes: [u8; 32] = BASE64_STANDARD
-            .decode(value)
-            .map_err(|_| WireguardError::InvalidPublicKey)?
-            .try_into()
-            .map_err(|_| WireguardError::InvalidPublicKey)?;
-
-        Ok(Self {
-            key: XPublicKey::from(bytes),
-        })
+        Self::try_from(value.as_str())
     }
 }
 
 impl From<&PrivateKey> for PublicKey {
     fn from(value: &PrivateKey) -> Self {
-        Self {
-            key: XPublicKey::from(&value.secret),
-        }
+        Self(XPublicKey::from(&value.0))
     }
 }
