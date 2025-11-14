47568bccb13ce94b669a4e08517b9f386ee4042a
Dmitry <idkncc@icloud.com>
2025-11-14 19:15:03 +0300

    feat(tests): serde for keys

--- tests/keys.rs
+++ tests/keys.rs
@@ -1,5 +1,12 @@
 use wireguard_conf::prelude::*;
 
+#[cfg(feature = "serde")]
+use claims::assert_ok_eq;
+#[cfg(feature = "serde")]
+use serde::Serialize;
+#[cfg(feature = "serde")]
+use serde_assert::{Serializer, Token};
+
 #[test]
 pub fn private_key() {
     let private_key = PrivateKey::random();
@@ -24,6 +31,18 @@ pub fn private_key() {
     );
 }
 
+#[cfg(feature = "serde")]
+#[test]
+pub fn private_key_serde() {
+    let private_key = PrivateKey::random();
+    let serializer = Serializer::builder().build();
+
+    assert_ok_eq!(
+        private_key.serialize(&serializer),
+        [Token::Str(private_key.to_string())]
+    );
+}
+
 #[test]
 pub fn public_key() {
     let private_key = PrivateKey::random();
@@ -40,6 +59,19 @@ pub fn public_key() {
     );
 }
 
+#[cfg(feature = "serde")]
+#[test]
+pub fn public_key_serde() {
+    let private_key = PrivateKey::random();
+    let public_key = PublicKey::from(&private_key);
+    let serializer = Serializer::builder().build();
+
+    assert_ok_eq!(
+        public_key.serialize(&serializer),
+        [Token::Str(public_key.to_string())]
+    );
+}
+
 #[test]
 pub fn preshared_key() {
     let preshared_key = PresharedKey::random();
@@ -52,5 +84,17 @@ pub fn preshared_key() {
     assert_eq!(
         PresharedKey::try_from(preshared_key_str.to_string()),
         Ok(preshared_key.clone())
+    );
+}
+
+#[cfg(feature = "serde")]
+#[test]
+pub fn preshared_key_serde() {
+    let preshared_key = PresharedKey::random();
+    let serializer = Serializer::builder().build();
+
+    assert_ok_eq!(
+        preshared_key.serialize(&serializer),
+        [Token::Str(preshared_key.to_string())]
     );
 }
